20541eb1a653f49c4eff446068a956ab4312caa9
ricardosugano <sugano.ricardo@gmail.com>
2026-08-18 22:52:59 -0300

    n

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,34 @@
-import { Text } from 'react-native';
-import { View } from 'react-native-reanimated/lib/typescript/Animated';
-import { red } from 'react-native-reanimated/lib/typescript/Colors';
+import React from 'react';
+import { View, Text, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { styles } from './styles';
+
 
 export default function Home() {
     return (
-        <View>
-            <Text style={{ color: 'red'}}>Hello, World!</Text>
-            <Text>Hello, World!</Text>
-        </View>
-    );
-}
+        <SafeAreaView style={styles.safeAreaView}>
+            <View style={styles.container}>
+                <Image 
+                    source={require('./assets/logo.png')} 
+                    style={styles.image} 
+                    resizeMode='contain'
+                    
+                />
+                
+                <Text style={styles.titulo}>TaskFlow</Text>
 
-const styles =  StyleSheet.create({
-    redText: {
-        color: 'red',
-        fontSize: 20
+                <Text>TaskFlow</Text>
+
+                <Text>Organize suas tarefas de forma eficiente</Text>
+            </View>
+        </SafeAreaView>
+    );
+}   
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        borderColor: 'black',
+        borderWidth: 1,
     }
 });
---
+++ app/styles.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    titulo: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        flex: 1,
+        justifyContent: 'center',
+        borderColor: 'black',
+        borderWidth: 1,
+    },
+    safeAreaView: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    descricao: {
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 10,
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginBottom: 20,
+    },
+    Logo: {
+        width: 100,
+        height: 100,
+        marginBottom: 20,
+    },
+});
